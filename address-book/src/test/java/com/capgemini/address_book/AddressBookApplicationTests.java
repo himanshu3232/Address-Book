cd9190c15130f726918bd3884b1507f6900d83b8
himanshu3232 <himanshu2000ranjan@gmail.com>
2025-02-24 12:21:09 +0530

    [Himanshu Ranjan] init

---
+++ address-book/src/test/java/com/capgemini/address_book/AddressBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.address_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
