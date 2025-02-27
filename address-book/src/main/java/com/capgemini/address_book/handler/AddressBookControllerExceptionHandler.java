b14a39535ba3bc47dd61990b464ee56584076ab2
himanshu3232 <himanshu2000ranjan@gmail.com>
2025-02-28 02:03:06 +0530

    [Himanshu Ranjan] Add. added controller advice

---
+++ address-book/src/main/java/com/capgemini/address_book/handler/AddressBookControllerExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.capgemini.address_book.handler;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class AddressBookControllerExceptionHandler {
+}
