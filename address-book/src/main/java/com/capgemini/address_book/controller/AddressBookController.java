dbe717b2cb2f77bf76c546d1e604a22ca130b092
himanshu3232 <himanshu2000ranjan@gmail.com>
2025-02-27 21:52:57 +0530

    [Himanshu Ranjan] Add. branches merged

--- address-book/src/main/java/com/capgemini/address_book/controller/AddressBookController.java
+++ address-book/src/main/java/com/capgemini/address_book/controller/AddressBookController.java
@@ -4,6 +4,7 @@ import com.capgemini.address_book.dto.AddressBookDto;
 import com.capgemini.address_book.model.AddressBook;
 import com.capgemini.address_book.service.IAddressBookService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,25 +12,32 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/address-book")
 @RequiredArgsConstructor
+@Slf4j
 public class AddressBookController {
 
     private final IAddressBookService addressBookService;
 
     @GetMapping("/get/{id}")
     public ResponseEntity<AddressBookDto> getAddressBook(@PathVariable Long id){
+        log.info("Get address book request with id : {}", id);
         return new ResponseEntity<>(addressBookService.getAddressBookById(id), HttpStatus.OK);
     }
 
     @PostMapping("/post")
     public ResponseEntity<AddressBookDto> postAddressBook(@RequestBody AddressBook addressBook){
+        log.info("post request for adding new address book");
+        log.trace("AddressBook: {} requested to be added", addressBook);
         return new ResponseEntity<>(addressBookService.addAddressBook(addressBook), HttpStatus.CREATED);
     }
     @PatchMapping("/update")
     public ResponseEntity<AddressBookDto> patchAddressBook(@RequestBody AddressBook addressBook){
+        log.info("Request to patch address book with id : {}", addressBook.getId());
+        log.trace("AddressBook: {} to be updated", addressBook);
         return new ResponseEntity<>(addressBookService.patchAddressBook(addressBook), HttpStatus.OK);
     }
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Boolean> deleteAddressBook(@PathVariable Long id){
+        log.info("Delete request to delete address book with id: {}", id);
         return new ResponseEntity<>(addressBookService.deleteAddressBookById(id), HttpStatus.ACCEPTED);
     }
 }
--- address-book/src/main/java/com/capgemini/address_book/service/impl/AddressBookServiceImpl.java
+++ address-book/src/main/java/com/capgemini/address_book/service/impl/AddressBookServiceImpl.java
@@ -6,12 +6,14 @@ import com.capgemini.address_book.model.AddressBook;
 import com.capgemini.address_book.repository.AddressBookRepository;
 import com.capgemini.address_book.service.IAddressBookService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Profile({"prod"})
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class AddressBookServiceImpl implements IAddressBookService {
 
     private final AddressBookRepository addressBookRepository;
@@ -19,6 +21,7 @@ public class AddressBookServiceImpl implements IAddressBookService {
 
     @Override
     public AddressBookDto addAddressBook(AddressBook addressBook) {
+        log.debug("AddressBook saved to database");
         AddressBook addedAddressBook = addressBookRepository.save(addressBook);
         return AddressBookDtoMapper.mapToAddressBookDto(addedAddressBook);
     }
@@ -26,17 +29,20 @@ public class AddressBookServiceImpl implements IAddressBookService {
     @Override
     public AddressBookDto getAddressBookById(Long id) {
         AddressBook addressBook = addressBookRepository.findById(id).orElseThrow();
+        log.debug("AddressBook with id: {} is {}", id, addressBook);
         return AddressBookDtoMapper.mapToAddressBookDto(addressBook);
     }
 
     @Override
     public AddressBookDto patchAddressBook(AddressBook addressBook) {
         AddressBook updatedAddressBook = addressBookRepository.save(addressBook);
+        log.debug("AddressBook patched successfully!");
         return AddressBookDtoMapper.mapToAddressBookDto(updatedAddressBook);
     }
 
     @Override
     public boolean deleteAddressBookById(Long id) {
+        
         addressBookRepository.deleteById(id);
         return true;
     }
